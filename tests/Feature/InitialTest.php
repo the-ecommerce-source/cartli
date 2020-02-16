a48147c36dc5ce39138d57e089e59a02dfd7ccb8
bakhtawarshah <cartlicms@gmail.com>
2020-02-16 23:35:20 +0400

    added test

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace TES\Cartli\Tests;
+
+use Orchestra\Testbench\TestCase;
+class InitialTest extends TestCase
+{
+    public function my_first_test()
+    {
+        $this->assertTrue(true);
+    }
+}
